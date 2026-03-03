eda6a66e106c34ff55521211c8d62a7bd8c13c03
Giovana-Rossi <giovana.rossi@aluno.ifsp.edu.br>
2026-03-02 21:27:14 -0300

    ex10

--- src/main/java/br/edu/ifsp/list01/Ex10.java
+++ src/main/java/br/edu/ifsp/list01/Ex10.java
@@ -32,8 +32,18 @@ public class Ex10 {
     }
 
     String compute(int x1, int v1, int x2, int v2) {
-        String output = null;
-        //put your logic here
-        return output;
+        if (v1 == v2)
+            if (x1 == x2) return "SIM";
+            else return "NAO";
+
+        if ((x2 - x1) % (v1 - v2) != 0)
+            return "NAO";
+
+        if ((x2 - x1) / (v1 - v2) < 0)
+            return "NAO";
+
+
+        return "SIM";
+
     }
 }
